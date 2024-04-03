ccb2b0879f9e776443ea317d2315dca969854a6a
Thomas Donahue <donahue777@yahoo.com>
2024-04-03 00:46:48 -0400

    Dapper initiated

--- Program.cs
+++ Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Data;
+using System.IO;
+using MySql.Data.MySqlClient;
+using Microsoft.Extensions.Configuration;
+
+namespace IntroSQL
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            var config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+            string connString = config.GetConnectionString("DefaultConnection");
+            IDbConnection conn = new MySqlConnection(connString);
+
+            DepartmentRepository repo = new DepartmentRepository(conn);
+            var Departments = repo.GetAllDepartments();
+
+            foreach(var dep in Departments)
+            {
+                Console.WriteLine($"{dep.DepartmentID} {dep.Name}");
+            }
+        }
+    }
+}
